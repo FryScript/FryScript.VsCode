dd674a737996a67a5d4cc89d6a773a7c5eb0fdfe
James Herring <james.herring@james.herring.com>
2018-11-29 13:55:08 +0000

    Added language server

---
+++ VsCode/LanguageServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LanguageServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
