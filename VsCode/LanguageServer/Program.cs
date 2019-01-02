27d80f33f6dd061854c15e12038d8df90fb8b34d
James Herring <james.herring@james.herring.com>
2019-01-02 14:01:34 +0000

    Added item completion kind

--- VsCode/LanguageServer/Program.cs
+++ VsCode/LanguageServer/Program.cs
@@ -374,7 +374,9 @@ namespace LanguageServer2
         {
             var completionItems = _analyser.GetIdentifiers(@params.TextDocument.Uri).Select(i => new CompletionItem
             {
-                Label = i
+                Label = i,
+                Kind = CompletionItemKind.Variable
+                
             }).ToArray();
 
             client.Response(completionItems);
@@ -480,11 +482,40 @@ namespace LanguageServer2
         TriggerForIncompleteCompletions = 3
     }
 
+    public enum CompletionItemKind
+    {
+        Text = 1,
+        Method = 2,
+        Function = 3,
+        Constructor = 4,
+        Field = 5,
+        Variable = 6,
+        Class = 7,
+        Interface = 8,
+        Module = 9,
+        Property = 10,
+        Unit = 11,
+        Value = 12,
+        Enum = 13,
+        Keyword = 14,
+        Snippet = 15,
+        Color = 16,
+        File = 17,
+        Reference = 18,
+        Folder = 19,
+        EnumMember = 20,
+        Constant = 21,
+        Struct = 22,
+        Event = 23,
+        Operator = 24,
+        TypeParameter = 25
+    }
+
     public class CompletionItem
     {
         public string Label;
 
-        public int Kind;
+        public CompletionItemKind Kind;
 
         public string Detail;
 
